810ed47bd8132dd5148172d44d25d008b6fe976f
Raul Francia <refc_80@hotmail.com>
2026-06-08 14:51:41 -0400

    Third Commit

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -83,7 +83,7 @@ export const dummyChats = [
     {
         "_id": "689de4bbaa932dc3a8ef6cd7",
         "userId": "689c6deed410acddc0d95a0e",
-        "userName": "GreatStack",
+        "userName": "AgoraGPT",
         "name": "New Chat",
         "messages": [
             {
@@ -267,13 +267,13 @@ export const dummyChats = [
         ],
         "createdAt": "2025-08-13T17:29:52.421Z",
         "updatedAt": "2025-08-14T09:39:19.046Z",
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "_id": Date.now(),
         "userId": "gs123456789",
         "name": "New Chat",
-        "userName": "GreatStack",
+        "userName": "AgoraGPT",
         "messages": [],
         "createdAt": "2025-08-13T17:29:52.421Z",
         "updatedAt": "2025-08-14T09:39:19.046Z",
@@ -283,46 +283,46 @@ export const dummyChats = [
 export const dummyPublishedImages = [
     {
         "imageUrl": ai_image11,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image10,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image9,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image8,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image7,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image6,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image5,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image4,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image3,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image2,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     },
     {
         "imageUrl": ai_image1,
-        "userName": "GreatStack"
+        "userName": "AgoraGPT"
     }
 ]
