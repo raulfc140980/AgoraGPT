24ca68b73ccf7a93efe495b5b9be61e3a5d8b33a
Raul Francia <refc_80@hotmail.com>
2026-06-08 15:43:12 -0400

    Ninth Commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,20 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
+const connectDB = async () =>{
     try {
-
-        await mongoose.connect(
-            `${process.env.MONGODB_URI}/agora_gpt`
-        );
-
-        console.log("MongoDB Connected");
-
+        mongoose.connection.on('connected', ()=> console.log('Database connected'))
+        await mongoose.connect(`${process.env.MONGODB_URI}/agora_gpt`)
     } catch (error) {
-
-        console.error("MongoDB Error:", error);
-
-        process.exit(1);
+        console.log(error.message)
     }
-};
+}
 
 export default connectDB;
